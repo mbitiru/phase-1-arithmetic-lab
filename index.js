4e7f161d3822f16c3d176fb8cddbea04bdb46a2b
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-18 18:37:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//either use const or let
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor( Math.random() * 100 ) + 1;
+
+const num3 = 16;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max( 2, 6, 12, 20);
